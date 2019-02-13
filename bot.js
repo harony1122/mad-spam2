003f36cbfba3e7e7cd4c03f550de53449c8c62ea
harony1122 <45592826+harony1122@users.noreply.github.com>
2019-02-13 07:23:38 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,66 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const client2 = new Discord.Client();
+const client3 = new Discord.Client();
+const client4 = new Discord.Client();
+const client5 = new Discord.Client();
+
+
+client.on('ready', () => {
+   console.log(`----------------`);
+   console.log(`Credit Farmm - Script By : Kahrbaa `);
+   console.log(`----------------`);
+   console.log(`Loadinng`);
+   console.log(`Loadinng.`);
+   console.log(`Loadinng..`);
+   console.log(`Loadinng...`);
+   console.log(`This Bots Online ' `);
+   console.log(`----------------`);
+});
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("536852233824763915")
+setInterval(function() {
+channel.send(`haroonylove`);
+}, 30)
+})
+
+
+client2.on("ready", () => {
+let channel =     client.channels.get("536852233824763915")
+setInterval(function() {
+channel.send(`haroonylove`);
+}, 30)
+})
+
+
+client3.on("ready", () => {
+let channel =     client.channels.get("536852233824763915")
+setInterval(function() {
+channel.send(`haroonylove`);
+}, 30)
+})
+
+
+client4.on("ready", () => {
+let channel =     client.channels.get("536852233824763915")
+setInterval(function() {
+channel.send(`haroonylove`);
+}, 30)
+})
+
+
+client5.on("ready", () => {
+let channel =     client.channels.get("536852233824763915")
+setInterval(function() {
+channel.send(`haroonylove`);
+}, 30)
+})
+
+
+client.login(process.env.TOKEN);// لا تغير فيها شيء
+client2.login(process.env.TOKEN2);//
+client3.login(process.env.TOKEN3);//
+client4.login(process.env.TOKEN4);//
+client5.login(process.env.TOKEN5);//
